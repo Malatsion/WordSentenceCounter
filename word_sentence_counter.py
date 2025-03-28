da1dd9422f65053bb1a159683985599e673b193c
Malatsion <m.malaction06@gmail.com>
2025-03-28 09:53:21 +0200

    Додавання основної функції count_words_sentences

---
+++ word_sentence_counter.py
@@ -0,0 +1,19 @@
+import re
+
+
+def count_words_sentences(file_path):
+    """
+    Функція зчитує вміст текстового файлу та підраховує кількість слів і речень.
+    :param file_path: шлях до файлу
+    :return: кортеж (кількість слів, кількість речень)
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    words = re.split(r'[\s,;:]+', text.strip())
+    words = [word for word in words if word]  # Видаляємо порожні елементи
+
+    sentences = re.split(r'(?<!\.)[.!?]+\s', text.strip())
+    sentences = [sentence for sentence in sentences if sentence]  # Видаляємо порожні елементи
+
+    return len(words), len(sentences)
